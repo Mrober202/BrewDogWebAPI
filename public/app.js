7b63b2a1d2dbb7977f58fa7cae6af2566579241c
Michael Robertson <michael_robertson_92@hotmail.co.uk>
2017-08-02 19:41:53 +0100

    brew dog web app

---
+++ public/app.js
@@ -0,0 +1,57 @@
+var app = function(){
+  var url = "https://api.punkapi.com/v2/beers";
+  makeRequest(url, requestComplete)
+}
+
+var makeRequest = function(url, callback) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener("load", callback);
+  request.send();
+};
+
+var requestComplete = function() {
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var beers = JSON.parse(jsonString);
+  var beer = beers[0];
+  populateList(beers);
+};
+
+var populateList = function(beers) {
+  var select = document.querySelector("select");
+  beers.forEach(function(beer) {
+    var li = document.createElement("option");
+    li.text = beer.name;
+    select.appendChild(li);
+  })
+  select.addEventListener("change", function(){
+    showList(beers[this.selectedIndex - 1]);
+    save(beers[this.selectedIndex - 1]);
+  })
+};
+
+var showList = function(beer) {
+  var ul = document.getElementById("beer-list");
+  ul.innerHTML = '';
+  var listItem1 = document.createElement("ul");
+  listItem1.innerText = "Name: " + beer.name 
+  var listItem2 = document.createElement("ul");
+  listItem2.innerText = "Description: " + beer.description 
+  var listItem3 = document.createElement("ul");
+  listItem3.innerText = "Percentage: " + beer.abv + "%"
+  var listItem4 = document.createElement("img");
+  listItem4.src = beer.image_url  
+  listItem4.className = "photo";
+
+  ul.appendChild(listItem1);
+  ul.appendChild(listItem2);
+  ul.appendChild(listItem3);
+  ul.appendChild(listItem4);
+  }
+
+  var save = function() {
+
+  }
+
+window.addEventListener('load', app);
